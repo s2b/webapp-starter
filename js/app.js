eeed323f8f4acf970fead2af675e6596f29c07d6
s2b <info@s2b-project.de>
2012-06-11 17:06:46 +0200

    fixed touch demo for text input

--- js/app.js
+++ js/app.js
@@ -27,19 +27,33 @@
 		if (touchDemo) {
 			$(document.body).append('<div id="touch-demo"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>');
 			var $touchDemo = $('#touch-demo').children();
-			
+
+			function touchDemoUpdate(e) {
+				for (var i = 0, length = e.touches.length; i < length; i++) {
+					$touchDemo.eq(i).css({
+						left: e.touches[i].pageX,
+						top: e.touches[i].pageY
+					}).show();
+				}
+			}
+
 			$(document)
-				.on('touchstart touchmove', function (e) {
-					for (var i = 0, length = e.touches.length; i < length; i++) {
-						$touchDemo.eq(i).css({
-							left: e.touches[i].pageX,
-							top: e.touches[i].pageY
-						}).show();
+				.on('touchstart', function (e) {
+					// Skip text input
+					var $target = $(e.target);
+					if ($target.is('textarea') || ($target.is('input') && $.inArray($target.attr('type'), ['submit', 'reset', 'button', 'image', 'checkbox', 'radio', 'range'])) < 0) {
+						return;
 					}
-				})
-				.on('touchend touchcancel', function () {
-					$touchDemo.hide();
-				});
+
+					touchDemoUpdate(e);
+					
+					$(document)
+						.on('touchmove', touchDemoUpdate)
+						.on('touchend touchcancel', function () {
+							$(document).off('touchmove', touchDemoUpdate);
+							$touchDemo.hide();
+						});
+				});				
 		}
 	}
 
